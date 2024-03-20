a40c40aef6177f4005a987550e0bdad58a412b85
Logan Rupert <logan.rupert@databricks.com>
2024-03-20 13:17:06 -0500

    fix path accesses.

--- ingestion/01-Ingestion-DLT.py
+++ ingestion/01-Ingestion-DLT.py
@@ -10,14 +10,14 @@ from utils.ingestionHelper import download_unzip_and_save_as_table
 # Assuming 'shots_2023.csv' is the name of the file inside the 'shots_2023.zip'
 
 shots_url = spark.conf.get("base_download_url") + "shots_2023.zip"
-dbfs_table_path = spark.conf.get("dbfs_table_path")
+tmp_base_path = spark.conf.get("tmp_base_path")
 table_name = "shots_2023"
 
 
 # COMMAND ----------
 @dlt.table(name="bronze_shots_2023", comment="Raw Ingested NHL data on Shots")
 def ingest_zip_data():
-    dbfs_file_path = download_unzip_and_save_as_table(
-        shots_url, dbfs_table_path, table_name, file_format=".zip"
+    tmp_base_path = download_unzip_and_save_as_table(
+        shots_url, tmp_base_path, table_name, file_format=".zip"
     )
-    return spark.read.format("csv").option("header", "true").load(dbfs_file_path)
+    return spark.read.format("csv").option("header", "true").load(tmp_base_path)
--- utils/ingestionHelper.py
+++ utils/ingestionHelper.py
@@ -4,7 +4,7 @@ import zipfile
 from pyspark.sql import SparkSession
 
 
-def download_unzip_and_save_as_table(url, dbfs_table_path, table_name, file_format):
+def download_unzip_and_save_as_table(url, tmp_base_path, table_name, file_format):
     """
     Downloads a ZIP file from a URL, checks if it is a ZIP file, unzips it, and saves the contents as a table in DBFS storage.
 
@@ -15,9 +15,9 @@ def download_unzip_and_save_as_table(url, dbfs_table_path, table_name, file_form
 
     # Temporary file path for downloading
     if file_format == ".zip":
-        temp_path = "/tmp/" + table_name + ".zip"
+        temp_path = tmp_base_path + table_name + ".zip"
     elif file_format == ".csv":
-        temp_path = dbfs_table_path + "/" + table_name + ".csv"
+        temp_path = tmp_base_path + table_name + ".csv"
 
     # Download the file
     with requests.get(url, stream=True) as r:
@@ -29,17 +29,13 @@ def download_unzip_and_save_as_table(url, dbfs_table_path, table_name, file_form
     if zipfile.is_zipfile(temp_path):
         with zipfile.ZipFile(temp_path, "r") as zip_ref:
             # Extract all the contents into the DBFS table path
-            zip_ref.extractall(dbfs_table_path)
+            zip_ref.extractall(tmp_base_path)
             print(
-                f"The file downloaded from {url} was a ZIP file and successfully copied to {temp_path}."
+                f"The file downloaded from {url} was a ZIP file and successfully copied to {tmp_base_path}."
             )
     else:
         print(
             f"The file downloaded from {url} is not a ZIP file and successfully copied to {temp_path}."
         )
 
-    print(
-        f"Table '{table_name}' successfully saved to DBFS at location: {dbfs_table_path}"
-    )
-
-    return dbfs_table_path
+    return temp_path
