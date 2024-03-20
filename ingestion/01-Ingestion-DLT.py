06c2c9896f8ca4ae533b50135e5abff85e6ff997
Logan Rupert <logan.rupert@databricks.com>
2024-03-20 12:45:07 -0500

    update ingestion DLT.

--- ingestion/01-Ingestion-DLT.py
+++ ingestion/01-Ingestion-DLT.py
@@ -8,8 +8,9 @@ from utils.ingestionHelper import download_unzip_and_save_as_table
 # COMMAND ----------
 # Example usage:
 # Assuming 'shots_2023.csv' is the name of the file inside the 'shots_2023.zip'
-url = "https://peter-tanner.com/moneypuck/downloads/shots_2023.zip"
-dbfs_table_path = "/mnt/Users/logan.rupert@databricks.com/nhl/raw/"
+
+shots_url = spark.conf.get("base_download_url") + "/shots_2023.zip"
+dbfs_table_path = spark.conf.get("dbfs_table_path")
 table_name = "shots_2023"
 
 
@@ -17,11 +18,6 @@ table_name = "shots_2023"
 @dlt.table(name="bronze_shots_2023", comment="Raw Ingested NHL data on Shots")
 def ingest_zip_data():
     dbfs_file_path = download_unzip_and_save_as_table(
-        url, dbfs_table_path, table_name, file_format=".zip"
+        shots_url, dbfs_table_path, table_name, file_format=".zip"
     )
-    return (
-        spark.read.format("csv")
-        .option("header", "true")
-        .mode("overwrite")
-        .load(dbfs_file_path)
-    )
+    return spark.read.format("csv").option("header", "true").load(dbfs_file_path)
