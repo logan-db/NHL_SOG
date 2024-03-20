5bc2c48019128d821a75c12072227e5ebd309cb9
logan.rupert@databricks.com <logan.rupert@databricks.com>
2024-03-20 19:21:43 +0000

    adding logs

--- ingestion/01-Ingestion-DLT.py
+++ ingestion/01-Ingestion-DLT.py
@@ -1,11 +1,11 @@
 # Databricks notebook source
-
 # Imports
 import dlt
 from pyspark.sql.functions import *
 from utils.ingestionHelper import download_unzip_and_save_as_table
 
 # COMMAND ----------
+
 # Example usage:
 # Assuming 'shots_2023.csv' is the name of the file inside the 'shots_2023.zip'
 
@@ -14,13 +14,16 @@ tmp_base_path = spark.conf.get("tmp_base_path")
 table_name = "shots_2023"
 
 # COMMAND ----------
-print(dbutils.fs.ls("/"))
+
+print(f"TESTING ACCESS ------ {dbutils.fs.ls('/Users/')}")
 
 # COMMAND ----------
-print(spark.table("lr_nhl_demo.dev.teams_2022").count())
+
+print(f"TESTING CATALOG --------  {spark.table('lr_nhl_demo.dev.teams_2022').count()}")
 
 
 # COMMAND ----------
+
 @dlt.table(name="bronze_shots_2023", comment="Raw Ingested NHL data on Shots")
 def ingest_zip_data():
     shots_file_path = download_unzip_and_save_as_table(
