ac6421db9728493082a80c07ad2cfaaab3bd60b8
Logan Rupert <logan.rupert@databricks.com>
2024-08-12 18:04:32 -0500

    reference files and cleaning

--- nhlPredict/src/01-Ingestion-createVols.sql
+++ nhlPredict/src/01-Ingestion-createVols.sql
@@ -1,4 +1,13 @@
 -- Databricks notebook source
+CREATE CATALOG IF NOT EXISTS lr_nhl_demo
+-- COMMENT 'NHL Prediction catalog created by Databricks Asset Bundle'
+
+-- COMMAND ----------
+
+CREATE SCHEMA IF NOT EXISTS lr_nhl_demo.dev
+
+-- COMMAND ----------
+
 CREATE VOLUME IF NOT EXISTS lr_nhl_demo.dev.lines_2023;
 
 CREATE VOLUME IF NOT EXISTS lr_nhl_demo.dev.teams_2023;
